e197e486d090913757270109ae66a71d71c25932
nanades <lisnadsta2707@gmail.com>
2025-05-21 15:05:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.write("ayo olahraga bersama !"
 st.image("1747010510672.jpg", width=200)
 st.title("apklikasi sederhana")
 st.header("apklikasi mengecek nilai Genap/Ganjil")
-angka = st.number_input("Tulis sebuah Angka:", volue=0, step=1)
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
 
 if (angka % 2) == 0:
         st.write(f"{angka} adalah Bilangan Genap")
