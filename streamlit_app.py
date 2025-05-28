b4ee1cf07fe34c08e6df4eac04fafaff2ad1ea90
nanades <lisnadsta2707@gmail.com>
2025-05-28 14:55:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ if (angka % 2) == 0:
         st.write(f"{angka} adalah Bilangan Genap")
 else:
         st.write(f"{angka} adalah Bilangan Ganjil")
-        
+
+
 st.title("Aplikasi Sederhana")
 
 # Menggunakan layout kolom
